acaf20a5a773e974074f423ad2bf41ae57b39d78
Woojin Chae <woojeeny02@kaist.ac.kr>
2024-06-15 22:26:35 +0900

    Feat: Wrapper

---
+++ bandit_models/__init__.py
@@ -0,0 +1,6 @@
+from .linucb import LinUCBAlgorithm
+from .thompson_sampling import ThompsonSamplingAlgorithm
+from .epsilon_greedy import EpsilonGreedyAlgorithm
+from .ucb1 import UCB1Algorithm
+from .softmax import SoftmaxAlgorithm
+from .bayesucb import BayesUCBAlgorithm
